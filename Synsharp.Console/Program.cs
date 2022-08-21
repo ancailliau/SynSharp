de2e307505804456415118546b7bb90e823d16cd
Antoine Cailliau <antoinecailliau@gmail.com>
2022-08-21 13:45:52 +0200

    Introduce synsharp.console

---
+++ Synsharp.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using Spectre.Console;
+using Spectre.Console.Cli;
+
+public static class Program
+{
+    public static int Main(string[] args)
+    {
+        AnsiConsole.Markup("[underline red]Welcome to Synsharp Console[/]\n");
+        var app = new CommandApp<StormCommand>();
+        return app.Run(args);
+    }
+}
---
+++ Synsharp.Console/StormCommand.cs
@@ -0,0 +1,74 @@
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using Microsoft.Extensions.Logging;
+using Spectre.Console;
+using Spectre.Console.Cli;
+using Synsharp;
+
+public class StormCommand : AsyncCommand<StormCommand.Settings>
+{
+    public sealed class Settings : CommandSettings
+    {
+        [CommandOption("-u|--username")]
+        [DefaultValue("root")]
+        public string Username { get; init; }
+
+        [CommandOption("-p|--password")]
+        [DefaultValue("secret")]
+        public string Password { get; init; }
+        
+        [CommandOption("--url")]
+        [DefaultValue("https://localhost:8902")]
+        public string Url { get; set; }
+    }
+
+    public override async Task<int> ExecuteAsync([NotNull] CommandContext context, [NotNull] Settings settings)
+    {
+        using var loggerFactory = LoggerFactory.Create(builder =>
+        {
+            builder.SetMinimumLevel(LogLevel.Trace);
+            builder.AddConsole();
+        });
+        
+        var synapseClient = new SynapseClient(settings.Url, loggerFactory);
+        await synapseClient.LoginAsync(settings.Username, settings.Password);
+
+        while (true)
+        {
+            try
+            {
+                var query = AnsiConsole.Ask<string>("$> ");
+                if (query.Equals("exit") | query.Equals("quit"))
+                {
+                    return 0;
+                }
+                
+                var results = await synapseClient.StormAsync<SynapseObject>(query).ToListAsync();
+
+                var init = synapseClient.Init;
+                var fini = synapseClient.Fini;
+
+                foreach (var result in results)
+                {
+                    AnsiConsole.WriteLine(result.ToString() ?? string.Empty);
+                }
+
+                var plural = fini.Count > 1 ? "s" : "";
+                AnsiConsole.WriteLine($"complete. {fini.Count} node{plural} in {fini.Took} ms.");
+            }
+            catch (SynapseException e)
+            {
+                AnsiConsole.WriteException(e);
+            }
+            catch (SynapseError e)
+            {
+                AnsiConsole.WriteException(e);
+            }
+            finally
+            {
+                AnsiConsole.WriteLine();   
+            }
+        }
+        return 0;
+    }
+}
